4f1eaf2caa2339ec9001b0df0ffb189769e60a79
demmanou <dimitris.emmn@gmail.com>
2021-01-12 18:38:10 +0100

    Refactored to make us of the body-parser module

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ const envelopes = {
     },
 }
 
-// app.use(bodyParser.json());
+app.use(bodyParser.json());
 
 app.get('/', (req, res, next) => {
     res.send('Hello World!');
@@ -45,48 +45,35 @@ app.get('/envelopes', (req, res, next) => {
 
 // will work with JSON name: <name>, budget: <budget>
 app.post('/envelopes', (req, res, next) => {
-    let finalData = '';
-    req.on('data', (data) => {
-        finalData += data;
-    })
-    req.on('end', () => {
-        req.body = JSON.parse(finalData);
-        let reqBodyKeys = Object.keys(req.body);
-        // check if the POST request includes the proper fields
-        if (!(reqBodyKeys.includes('name') && reqBodyKeys.includes('budget'))) { return res.sendStatus(400) };
-        // console.log(req.body.name);
-        // check if the resource requested already exists
-        if (envelopes[req.body.name]) { return res.send('Resource already exists!') };
-        let envelopesArray = Object.keys(envelopes);
-        // console.log(envelopesArray)
-        let newId = envelopesArray.length + 1;
-        req.body.id = newId;
-        envelopes[req.body.name] = {
-            budget: req.body.budget,
-            id: req.body.id,
-        };
-        res.status(201).send(req.body.name + ' envelope added with ' + req.body.budget + ' as its budget and ' + req.body.id + ' as its id');
-    })
+    let reqBodyKeys = Object.keys(req.body);
+    // check if the POST request includes the proper fields
+    if (!(reqBodyKeys.includes('name') && reqBodyKeys.includes('budget'))) { return res.sendStatus(400) };
+    // console.log(req.body.name);
+    // check if the resource requested already exists
+    if (envelopes[req.body.name]) { return res.send('Resource already exists!') };
+    let envelopesArray = Object.keys(envelopes);
+    // console.log(envelopesArray)
+    let newId = envelopesArray.length + 1;
+    req.body.id = newId;
+    envelopes[req.body.name] = {
+        budget: req.body.budget,
+        id: req.body.id,
+    };
+    res.status(201).send(req.body.name + ' envelope added with ' + req.body.budget + ' as its budget and ' + req.body.id + ' as its id');
 })
 
 // should only accept a budget: <budget> format
 app.put('/envelopes/:envelopeName', (req, res, next) => {
-    let finalData = '';
-    req.on('data', (data) => {
-        finalData += data;
+    req.body = JSON.parse(finalData);
+    let requestedKeys = Object.keys(req.body);
+    console.log(requestedKeys[0]);
+    if (requestedKeys[0] !== 'budget') return res.sendStatus(400);
+    // let existingEnvelopes = Object.keys(envelopes);
+    // if (!requestedKeys.every(key => envelopes.existingEnvelopes[0].includes(key))) { return res.sendStatus(400) };
+    requestedKeys.forEach(key => {
+        envelopes[req.params.envelopeName][key] = req.body[key];
     })
-    req.on('end', () => {
-        req.body = JSON.parse(finalData);
-        let requestedKeys = Object.keys(req.body);
-        console.log(requestedKeys[0]);
-        if (requestedKeys[0] !== 'budget') return res.sendStatus(400);
-        // let existingEnvelopes = Object.keys(envelopes);
-        // if (!requestedKeys.every(key => envelopes.existingEnvelopes[0].includes(key))) { return res.sendStatus(400) };
-        requestedKeys.forEach(key => {
-            envelopes[req.params.envelopeName][key] = req.body[key];
-        })
-        res.send('Resource updated');
-    });
+    res.send('Resource updated');
 });
 
 app.delete('/envelopes/:envelopeName', (req, res, next) => {
@@ -102,18 +89,12 @@ app.get('/envelopes/:envelopeName', (req, res, next) => {
 })
 
 app.post('/envelopes/transfer/:from/:to', (req, res, next) => {
-    let finalData = '';
-    req.on('data', (data) => {
-        finalData += data;
-    })
-    req.on('end', () => {
-        req.body = JSON.parse(finalData);
-        let requestedKeys = Object.keys(req.body);
-        if (requestedKeys[0] !== 'budget') return res.sendStatus(400);
-        envelopes[req.params.from].budget -= Number(req.body.budget);
-        envelopes[req.params.to].budget += Number(req.body.budget);
-        res.send('Transfer performed');
-    });
+    req.body = JSON.parse(finalData);
+    let requestedKeys = Object.keys(req.body);
+    if (requestedKeys[0] !== 'budget') return res.sendStatus(400);
+    envelopes[req.params.from].budget -= Number(req.body.budget);
+    envelopes[req.params.to].budget += Number(req.body.budget);
+    res.send('Transfer performed');
 })
 
 app.listen(port, () => {
